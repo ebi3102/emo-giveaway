77a5fa44701b319c54b19b8f5a81064765351ef7
Ebrahim Moeini <ebi3102@gmail.com>
2023-05-08 16:42:01 +0330

    Create plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd828f32535e57bba9c07');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '697ca3d66215948eb0fb');
