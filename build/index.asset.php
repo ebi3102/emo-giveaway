4bf73d5042b74cf03ab21962853501836b33cdd4
Ebrahim Moeini <ebi3102@gmail.com>
2023-05-10 11:49:58 +0330

    build changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '697ca3d66215948eb0fb');
-=======
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b0ce3c7d0d8f3ad4c99c');
->>>>>>> simple-block
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b284dfced8465d17f356');
